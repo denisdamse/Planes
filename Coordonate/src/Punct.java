2c2545f191a52331993ad4ef51cf280f29ed89e9
Denis <denisdamse@gmail.com>
2024-04-02 16:49:44 +0300

    finished

---
+++ Coordonate/src/Punct.java
@@ -0,0 +1,8 @@
+public class Punct {
+    private int x,y;
+    public Punct(int X, int Y)
+    {
+        this.x=X;
+        this.y=Y;
+    }
+}
---
+++ Coordonate/src/PunctColorat.java
@@ -0,0 +1,9 @@
+public class PunctColorat extends Punct
+ {
+    String c;
+    public PunctColorat(String C, int X, int Y)
+    {
+        super(X,Y);
+        this.c=C;
+    }
+}
